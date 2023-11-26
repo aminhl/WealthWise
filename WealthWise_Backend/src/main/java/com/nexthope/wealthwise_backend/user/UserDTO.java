2d72e7fac517d16025b7f20cb64072f96d5af7a0
aminhl <amin.hlel@esprit.tn>
2023-11-26 21:07:04 +0100

    adding_userDTO

---
+++ WealthWise_Backend/src/main/java/com/nexthope/wealthwise_backend/user/UserDTO.java
@@ -0,0 +1,7 @@
+package com.nexthope.wealthwise_backend.user;
+
+public record UserDTO(String firstname,
+                      String lastname,
+                      String userEmail
+                      ) {
+}
