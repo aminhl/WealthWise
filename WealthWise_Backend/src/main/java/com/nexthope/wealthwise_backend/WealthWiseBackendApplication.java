e0953c8cefbe16fe2fc76ccdcc580ef69d351078
aminhl <amin.hlel@esprit.tn>
2023-11-26 15:47:37 +0100

    project_base_strucutre

---
+++ WealthWise_Backend/src/main/java/com/nexthope/wealthwise_backend/WealthWiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexthope.wealthwise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthWiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthWiseBackendApplication.class, args);
+	}
+
+}
---
+++ WealthWise_Backend/src/test/java/com/nexthope/wealthwise_backend/WealthWiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexthope.wealthwise_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthWiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
